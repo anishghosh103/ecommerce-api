5a976cf8a23a75a4298eac4935177b98d5a05480
Anish Ghosh <anishghosh103@gmail.com>
2018-05-20 17:11:55 +0530

    Implemented clear authorization error messages

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,15 +9,15 @@ const Admin = require('../models/Admin');
 let customerAuthenticated = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
-    res.json(response.error('You are not authorized to access this page.', 403));
+    res.json(response.error('You are not authorized to access this page. Login first.', 403));
   } else {
     jwt.verify(token, appConfig.secret, (err, decoded) => {
       if (err || !decoded || decoded.userType !== 'customer') {
-        res.json(response.error('You are not authorized to access this page.', 403));
+        res.json(response.error('You are not authorized to access this page. Login first.', 403));
       } else {
         Customer.findOne({ customerId: decoded.customerId }, (err, customer) => {
           if (err) {
-            res.json(response.error('You are not authorized to access this page.', 403));
+            res.json(response.error('You are not authorized to access this page. Login first.', 403));
           } else {
             req.customer = customer;
             next();
@@ -31,15 +31,15 @@ let customerAuthenticated = (req, res, next) => {
 let adminAuthenticated = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
-    res.json(response.error('You are not authorized to access this page.', 403));
+    res.json(response.error('Admin Permission Required. You are not authorized to access this page.', 403));
   } else {
     jwt.verify(token, appConfig.secret, (err, decoded) => {
       if (err || !decoded || decoded.userType !== 'admin') {
-        res.json(response.error('You are not authorized to access this page.', 403));
+        res.json(response.error('Admin Permission Required. You are not authorized to access this page.', 403));
       } else {
         Admin.findOne({ adminId: decoded.adminId }, (err, admin) => {
           if (err) {
-            res.json(response.error('You are not authorized to access this page.', 403));
+            res.json(response.error('Admin Permission Required. You are not authorized to access this page.', 403));
           } else {
             req.admin = admin;
             next();
